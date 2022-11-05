5a62967d7839987f7b19ca73afb055985caea5ae
Kewin Titus <kewintitus@gmail.com>
2022-11-05 17:38:07 +0530

    ...

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+//URL Query parameter
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  // if (id > tours.length) {
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'invalid id' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
