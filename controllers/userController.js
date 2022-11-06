f50ea09750bb454509458302bfa872b19271a39c
Kewin Titus <kewintitus@gmail.com>
2022-11-06 19:59:00 +0530

    File stucture of routes established

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'route not implemented',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'route not implemented',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'route not implemented',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'route not implemented',
+  });
+};
+
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'route not implemented',
+  });
+};
---
+++ routes/tourRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const tourController = require('./../controllers/tourController');
+
+const router = express.Router();
+
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id: ${val}`);
+  next();
+});
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
