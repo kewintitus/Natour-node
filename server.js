004fdf1e036efb8363f58b3fd884e7e1d65f282b
Kewin Titus <kewintitus@gmail.com>
2022-11-07 20:42:21 +0530

    defined tour schema

--- server.js
+++ server.js
@@ -22,6 +22,21 @@ mongoose
   });
 // console.log(process.env);
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
