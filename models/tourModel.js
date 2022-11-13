3eb50c06e5b9b0ffda76103218bccecf3bbc45a7
Kewin Titus <kewintitus@gmail.com>
2022-11-13 13:35:19 +0530

    added built in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'Tour must have a name'],
       unique: true,
       trim: true,
+      maxLength: [40, 'A tour name must have less than or equal 40 characters'],
+      minLength: [10, 'A tour name must have more than or equal 10 characters'],
     },
     slug: String,
     duration: {
@@ -21,8 +23,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'Tour must have difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy,medium or difficult',
+      },
     },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
     ratingsQuantity: { type: Number, default: 0 },
 
     price: {
