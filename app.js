5ccb5fe5768c4cec2a44e8719f8d31a7b15cbb94
Kewin Titus <kewintitus@gmail.com>
2022-11-26 13:29:03 +0530

    user authorization completed

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +35,26 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against no-sql query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 //Serving for static files
 app.use(express.static(`${__dirname}/public`));
