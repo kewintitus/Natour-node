bfeae6bda15b740f95a1180609a9ace5977ce700
Kewin Titus <kewintitus@gmail.com>
2022-11-29 10:35:17 +0530

    added nested tour route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,6 +4,10 @@ const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  //Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
+
 const router = express.Router();
 
 // router.param('id', tourController.checkId);
@@ -26,6 +28,14 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
