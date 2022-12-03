8bb43dc8c3e0c0f23c9cf43cd42fa370f3e0b378
Kewin Titus <kewintitus@gmail.com>
2022-12-03 19:58:17 +0530

    added routes for tourDetails page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,33 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+const { isValidObjectId } = require('mongoose');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Get tour data from collection
+  const tours = await Tour.find();
+
+  //2) Build template
+  //3) Render tour
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  console.log(tour);
+
+  // const reviews = await Review.find();
+  // console.log(reviews);
+
+  res.status(200).render('tour', {
+    title: `${req.params.slug.toUpperCase()}`,
+    tour,
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,14 +4,14 @@ const viewsController = require('../controllers/viewsController');
 const router = express.Router();
 
 //ROUTES
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
+// router.get('/', (req, res) => {
+//   res.status(200).render('base', {
+//     tour: 'The Forest Hiker',
+//     user: 'Jonas',
+//   });
+// });
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
